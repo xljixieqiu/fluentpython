1e5ea75234f35e959c6121296f3031fbcc0b9829
xl <xljixieqiu@163.com>
2020-09-22 23:07:29 +0800

    fluentpython

---
+++ demo7_18.py
@@ -0,0 +1,10 @@
+from demo7_15 import clock
+import functools
+@functools.lru_cache()
+@clock
+def fibonacci(n):
+	if n<2:
+		return n
+	return fibonacci(n-2)+fibonacci(n-1)
+if __name__=='__main__':
+	print(fibonacci(6))
